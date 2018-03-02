3052843937119e35fa1d07e6047bbc7b78454bec
David Turanski <dturanski@pivotal.io>
2018-03-02 13:04:34 -0500

    update README and run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-if ! [ $(which python) ]; then
-    echo "Python not found."
-    exit 1
+PYTHON=python3
+
+if !command -v $PYTHON &>/dev/null; then
+    echo Python is not installed
 fi
 
-if ! [ $(which pip) ]; then
+if ! command -v pip &>/dev/null; then
    echo "Installing pip"
    wget https://bootstrap.pypa.io/get-pip.py
-   sudo python get-pip.py
+   sudo $PYTHON get-pip.py
    sudo pip install -U pip
    rm get-pip.py
 fi
 
-if ! [ $(which virtualenv) ]; then
-   echo "Installing virtualenv"
-   sudo pip install -U virtualenv
-fi
+REQUIRED_PYTHON_VERSION=3.6
+VENV_DIR=test_pyenv
 
-virtualenv -q -p "python$1" $2
-source $2/bin/activate
+virtualenv -q -p "python$REQUIRED_PYTHON_VERSION" $VENV_DIR
+source $VENV_DIR/bin/activate
 pip install -r invoker/requirements.txt
 
 ./setup.py test
 
 deactivate
 
-rm -rf $2
+rm -rf $VENV_DIR
